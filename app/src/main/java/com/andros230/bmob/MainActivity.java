23eb278189421e409f2459a8cad76195881fe4e9
andros230 <andros230@126.com>
2016-06-17 21:48:24 +0800

    查询数据

--- app/src/main/java/com/andros230/bmob/MainActivity.java
+++ app/src/main/java/com/andros230/bmob/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.List;
+
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化
         Bmob.initialize(this, "5b9353d27ae18dc5aafb5bf57b85a06b");
-        Person person = new Person();
+        //增加数据
+        final Person person = new Person();
         person.setName("lucky");
         person.setAddress("北京");
         person.save(this, new SaveListener() {
@@ -28,5 +34,23 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("---", "onFailure");
             }
         });
+
+        //查询数据
+        BmobQuery<Person> query = new BmobQuery<>();
+        query.findObjects(MainActivity.this, new FindListener<Person>() {
+            @Override
+            public void onSuccess(List<Person> list) {
+                for (Person person1 : list) {
+                    Log.d("查询---", person1.getName() + person1.getAddress());
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e("r---", "查询数据onError");
+            }
+        });
+
+
     }
 }
